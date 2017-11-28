5b875f3d04ff9c55f7695ca381f9511a0ff84ded
Phaedra144 <bedi.szilvia@gmail.com>
2017-11-28 16:05:20 +0100

    init(kalendaryo)initialise project, add application prop example

---
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/KalendaryoApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.opal.kalendaryo.kalendaryo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalendaryoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalendaryoApplication.class, args);
+	}
+}
