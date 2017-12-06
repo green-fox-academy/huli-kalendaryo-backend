c2f56827bcb7a0cc0426bb8477a398dd71593394
Marci <szekelymarton00@gmail.com>
2017-12-06 10:08:42 +0100

    created postmapping

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/MainController.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/MainController.java
@@ -1,9 +1,17 @@
 package com.greenfoxacademy.opal.kalendaryo.kalendaryo;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+
+  @PostMapping("")
+  public Object insertEvent() {
+
+
+  }
 
 }
