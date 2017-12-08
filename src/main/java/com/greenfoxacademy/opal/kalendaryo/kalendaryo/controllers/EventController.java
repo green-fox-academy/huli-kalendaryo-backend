18b0d1b2836c1bc8fe5aaa9127895fee7453bb79
Marci <szekelymarton00@gmail.com>
2017-12-08 09:39:13 +0100

    extended insert method

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/EventController.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/EventController.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.opal.kalendaryo.kalendaryo.controllers;
+import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.io.IOException;
@@ -10,6 +12,32 @@ import static com.greenfoxacademy.opal.kalendaryo.kalendaryo.authorization.Autho
 @Controller("/kalendaryo")
 public class EventController {
 
+
+    DateTime startDateTime = new DateTime("2015-05-28T09:00:00-07:00");
+    EventDateTime start = new EventDateTime()
+        .setDateTime(startDateTime)
+        .setTimeZone("America/Los_Angeles");
+
+    DateTime endDateTime = new DateTime("2015-05-28T17:00:00-07:00");
+    EventDateTime end = new EventDateTime()
+        .setDateTime(endDateTime)
+        .setTimeZone("America/Los_Angeles");
+
+    @PostMapping("/insert")
+    public void insertEvent() throws IOException {
+
+        com.google.api.services.calendar.Calendar service =
+            getCalendarService();
+        Event event = service.events().insert("primary", new Event()
+            .setDescription("New Event")
+            .setSummary("This is my first event")
+            .setStart(start)
+            .setEnd(end))
+            .execute();
+
+    }
+
+
     @GetMapping("/update")
     public void updateEvent() throws IOException {
         com.google.api.services.calendar.Calendar service =
@@ -17,12 +45,5 @@ public class EventController {
         Event event = service.events().get("primary", "7nhdmehd85ogf46u0sdcpmfq7h").execute();
         event.setSummary("Troollolloo!!!");
         Event updatedEvent = service.events().update("primary", event.getId(), event).execute();
-    }
-
-    @PostMapping("/insert")
-    public void insertEvent() throws IOException {
-        com.google.api.services.calendar.Calendar service =
-            getCalendarService();
-        Event event = service.events().insert("primary","").execute();
     }
 }
