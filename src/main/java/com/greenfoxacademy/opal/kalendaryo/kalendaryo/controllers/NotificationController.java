1bec4af69715daba616e03fff5c618d8e52ae17b
bekobarna <bocce15@gmail.com>
2017-12-11 13:47:57 +0100

    Notification response endpoint

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/NotificationController.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/NotificationController.java
@@ -6,9 +6,11 @@ import com.google.api.services.calendar.model.Channel;
 import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.EventChange;
 import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.EventResponse;
 import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
@@ -16,30 +18,19 @@ public class NotificationController {
 
     EventChange eventChange;
 
-    @PostMapping("/noti")
+    /*@PostMapping("/noti")
     public EventResponse eventWatch(@RequestBody EventChange eventChange) throws IOException {
         com.google.api.services.calendar.Calendar service = getCalendarService();
         Channel channel = new Channel();
         Watch eventWatch = service.events().watch(eventChange.getId(), channel);
         eventWatch.execute();
         return new EventResponse("api#channel", channel.getId(), eventChange.getId(), "https://www.googleapi.com/calendar/v3/calendars/huli.opal.kalendaryo@gmail.com/events/watch" );
+    }*/
+
+    @GetMapping(value = "/noti")
+    public void eventNotification(@RequestBody EventResponse eventResponse) {
+
+        System.out.println("The kind of the response: " + eventResponse.getKind() + "\n The ID of the notification channel: " + eventResponse.getId() + "\n The ID of the watched event" + eventResponse.getResourceId() + "\n The resourceUri of the watch: " + eventResponse.getResourceUri());
     }
-
-    /*@GetMapping(value = "/notification")
-    public void eventNotification() throws IOException {
-        com.google.api.services.calendar.Calendar service =
-            getCalendarService();
-        Channel channel = new Channel();
-        Watch eventWatch = service.events().watch("primary", channel);
-        System.out.println("hello");
-    }*/
-
-   /* @PostMapping(value = "https://www.googleapis.com/calendar/v3/calendars/{calendarId}/events/watch")
-    public EventResponse watchEvents (@PathVariable("calendarId") String calendarId,
-        @RequestBody EventChange eventChange) {
-
-        return new EventResponse("api#channel", "channelid", eventChange.getId(), "https://www.googleapis.com/calendar/v3/calendars/" + calendarId + "/events");
-    }*/
-
 
 }
