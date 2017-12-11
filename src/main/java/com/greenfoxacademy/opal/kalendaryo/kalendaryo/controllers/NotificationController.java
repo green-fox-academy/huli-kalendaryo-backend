80389985fe9e96007dcbcc3de12143b55a4a6c72
bekobarna <bocce15@gmail.com>
2017-12-11 14:26:29 +0100

    PushNotification correct endpoint

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/NotificationController.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/NotificationController.java
@@ -2,15 +2,17 @@ package com.greenfoxacademy.opal.kalendaryo.kalendaryo.controllers;
 
 
 import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.EventResponse;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+
+@RestController
 public class NotificationController {
 
-    @GetMapping(value = "/noti")
-    public void eventNotification(@RequestBody EventResponse eventResponse) {
+    @PostMapping(value = "/noti")
+    public EventResponse eventNotification(@RequestBody EventResponse eventResponse) {
         System.out.println("The kind of the response: " + eventResponse.getKind() + "\n The ID of the notification channel: " + eventResponse.getId() + "\n The ID of the watched event" + eventResponse.getResourceId() + "\n The resourceUri of the watch: " + eventResponse.getResourceUri());
+        return eventResponse;
     }
 }
