3146e08d9659ccb14006e3ae28ad5a8d784f049e
bekobarna <bocce15@gmail.com>
2018-01-02 12:57:21 +0100

    sql files in progress

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/KalendaryoApplication.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/KalendaryoApplication.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.opal.kalendaryo.kalendaryo;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +15,10 @@ public class KalendaryoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-//		Flyway flyway = new Flyway();
-//		flyway.setDataSource("jdbc:mysql://localhost:3306/kalendaryo_backend", "root", "12345");
+		Flyway flyway = new Flyway();
+		flyway.setDataSource("jdbc:mysql://localhost:3306/kalendaryo_backend", "root", "12345");
+		//flyway.baseline();
+		flyway.migrate();
 	}
 
 }
---
+++ src/main/resources/db/migration/V1__Initial_setup.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Auth_Model (
+  email varchar(255),
+  authCode varchar(255),
+  displayName varchar(255)
+);
