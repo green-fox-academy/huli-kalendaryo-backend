5f78810ad0dde14489a7a945806afb01b71120a0
bekobarna <bocce15@gmail.com>
2018-01-04 15:28:56 +0100

    flyway in commandlinerunner

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/KalendaryoApplication.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/KalendaryoApplication.java
@@ -1,8 +1,6 @@
 package com.greenfoxacademy.opal.kalendaryo.kalendaryo;
 
-import com.greenfoxacademy.opal.kalendaryo.kalendaryo.service.FlywayService;
 import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,15 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KalendaryoApplication implements CommandLineRunner {
 
-	@Autowired
-	FlywayService flywayService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(KalendaryoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		flywayService.flywayMigrate();
+
+		Flyway flyway = new Flyway();
+		String url = "jdbc:mysql://" + System.getenv("RDS_HOSTNAME") + ":" + System.getenv("RDS_PORT") + "/" + System.getenv("RDS_DB_NAME");
+		String username = System.getenv("RDS_USERNAME");
+		String password = System.getenv("RDS_PASSWORD");
+
+		flyway.setDataSource(url, username, password);
+		flyway.migrate();
 	}
 }
