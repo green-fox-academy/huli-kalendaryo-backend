9a5c7406dfd239e86414558e7ba0da9a3fad3d60
Marci <szekelymarton00@gmail.com>
2018-01-09 16:05:48 +0100

    added get list of mergedcal

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/MergedCalController.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/MergedCalController.java
@@ -4,6 +4,7 @@ import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.MergedCalendar;
 import com.greenfoxacademy.opal.kalendaryo.kalendaryo.repository.MergedCalendarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,12 @@ public class MergedCalController {
   public HttpStatus postMergedCal(@RequestBody MergedCalendar mergedCalendar) {
     mergedCalendarRepository.save(mergedCalendar);
     return HttpStatus.OK;
+  }
+
+  @GetMapping(value = "/listmergedcal")
+  public Iterable<MergedCalendar> getMergedCalList() {
+    Iterable<MergedCalendar> mergedCalendarsResponse = mergedCalendarRepository.findAll();
+    return  mergedCalendarsResponse;
   }
 
 }
