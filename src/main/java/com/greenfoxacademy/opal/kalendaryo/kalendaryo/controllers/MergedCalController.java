43e8167b3056572998acf3a05def7456b87266c4
Marci <szekelymarton00@gmail.com>
2018-01-09 16:04:38 +0100

    created mergedcal controller

---
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/MergedCalController.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.opal.kalendaryo.kalendaryo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MergedCalController {
+
+}
