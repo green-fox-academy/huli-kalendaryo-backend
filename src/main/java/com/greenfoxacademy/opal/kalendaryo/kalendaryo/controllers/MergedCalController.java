2c0cc33ae5613454876f856815cdd3e46a390da3
Marci <szekelymarton00@gmail.com>
2018-01-17 13:30:00 +0100

    added controller

---
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/MergedCalController.java
@@ -0,0 +1,31 @@
+package com.greenfoxacademy.opal.kalendaryo.kalendaryo.controllers;
+
+import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.MergedCalendar;
+import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.MergedCalendarResponse;
+import com.greenfoxacademy.opal.kalendaryo.kalendaryo.repository.MergedCalendarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MergedCalController {
+
+  @Autowired
+  MergedCalendarRepository mergedCalendarRepository;
+
+  @PostMapping(value = "/calendar")
+  public ResponseEntity postMergedCal(@RequestBody MergedCalendar mergedCalendar) {
+    mergedCalendarRepository.save(mergedCalendar);
+    return new ResponseEntity(HttpStatus.OK);
+  }
+
+  @GetMapping(value = "/calendar")
+  public MergedCalendarResponse getMergedCalList() {
+    return new MergedCalendarResponse();
+  }
+
+}
