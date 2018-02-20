dc77927e095e5c60af555b5283d5032e37fdf1e5
adel <navracsics.adel@gmail.com>
2018-02-20 10:05:35 +0100

    feat(notification tests): add missing resource id test

--- src/test/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/KalendaryoApplicationTests.java
+++ src/test/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/KalendaryoApplicationTests.java
@@ -28,19 +28,19 @@ import java.nio.charset.Charset;
 @EnableWebMvc
 public class KalendaryoApplicationTests {
 
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(),
-			Charset.forName("utf8"));
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
 
-	@Autowired
-	WebApplicationContext webApplicationContext;
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
-	private MockMvc mock;
+    private MockMvc mock;
 
-	@Before
-	public void setUp() throws Exception {
-		mock = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	}
+    @Before
+    public void setUp() throws Exception {
+        mock = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
     public void endPointExists() throws Exception {
@@ -48,8 +48,8 @@ public class KalendaryoApplicationTests {
                 .andExpect(status().isNotAcceptable());
     }
 
-	@Test
-	public void postNotificationGivesHttpStatusOk() throws Exception {
+    @Test
+    public void postNotificationGivesHttpStatusOk() throws Exception {
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Goog-Channel-ID", "01234567-89ab-cdef-0123456788");
         headers.add("X-Goog-Resource-ID", "WDOXEjsdYtXzZHq93mDhG6dfTrg");
@@ -59,16 +59,32 @@ public class KalendaryoApplicationTests {
         headers.add("X-Goog-Channel-Expiration", "1516102799000");
         headers.add("X-Goog-Channel-Token", "");
 
-		mock.perform(post("/notification")
-				.contentType(contentType)
-				.headers(headers)).andDo(print())
-				.andExpect(status().isOk());
-	}
+        mock.perform(post("/notification")
+                .contentType(contentType)
+                .headers(headers)).andDo(print())
+                .andExpect(status().isOk());
+    }
 
     @Test
-    public void channelLIdMissing() throws Exception {
+    public void channelIdMissing() throws Exception {
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Goog-Resource-ID", "WDOXEjsdYtXzZHq93mDhG6dfTrg");
+        headers.add("X-Goog-Resource-URI", "https://www.googleapis.com/calendar/v3/calendars/huli.opal.kalendaryo@gmail.com/events?maxResults=250&alt=json");
+        headers.add("X-Goog-Resource-State", "sync");
+        headers.add("X-Goog-Message-Number", "34");
+        headers.add("X-Goog-Channel-Expiration", "1516102799000");
+        headers.add("X-Goog-Channel-Token", "");
+
+        mock.perform(post("/notification")
+                .contentType(contentType)
+                .headers(headers)).andDo(print())
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    public void resourceIdMissing() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Goog-Channel-ID", "01234567-89ab-cdef-0123456788");
         headers.add("X-Goog-Resource-URI", "https://www.googleapis.com/calendar/v3/calendars/huli.opal.kalendaryo@gmail.com/events?maxResults=250&alt=json");
         headers.add("X-Goog-Resource-State", "sync");
         headers.add("X-Goog-Message-Number", "34");
