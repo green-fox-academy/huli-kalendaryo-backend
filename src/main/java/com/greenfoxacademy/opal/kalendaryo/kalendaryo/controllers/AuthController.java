cf129b240b9d019da5f9589d9e6672c3749fbade
Vajda Lilla <vajdalil.hu@gmail.com>
2018-02-20 13:33:46 +0100

    refactor(AuthController had a typo

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/AuthController.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/controllers/AuthController.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-public class AuthContoller {
+public class AuthController {
 
     @Autowired
     AuthAndUserService authAndUserService;
