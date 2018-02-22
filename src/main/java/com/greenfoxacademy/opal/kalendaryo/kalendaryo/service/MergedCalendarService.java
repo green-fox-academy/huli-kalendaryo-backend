cd421942985877b3af74749cc48fb64cc493e9f8
Tung <tung@Macintosh-MacBook-Pro-9.local>
2018-02-22 17:27:51 +0100

    add MergedCalendar service and GetCalendar endpoint

---
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/service/MergedCalendarService.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.opal.kalendaryo.kalendaryo.service;
+
+import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.entity.UserModel;
+import com.greenfoxacademy.opal.kalendaryo.kalendaryo.repository.MergedCalendarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MergedCalendarService {
+    @Autowired
+    MergedCalendarRepository mergedCalendarRepository;
+
+    public UserModel findMergedCalendars(UserModel user) {
+        user.setMergedCalendarList(mergedCalendarRepository.findMergedCalendarsByUserName(user));
+        return user;
+    }
+
+}
