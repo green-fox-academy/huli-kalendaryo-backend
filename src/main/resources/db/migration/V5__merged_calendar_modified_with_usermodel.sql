05227ab7aa336737825d19fef311d4fb55d90857
Hieu <hieuci88@gmail.com>
2018-03-06 14:29:53 +0100

    V5 file reset

--- src/main/resources/db/migration/V5__merged_calendar_modified_with_usermodel.sql
+++ src/main/resources/db/migration/V5__merged_calendar_modified_with_usermodel.sql
@@ -1,6 +1,3 @@
 ALTER TABLE merged_calendar
     CHANGE user_name user BIGINT,
     ADD FOREIGN KEY (user) REFERENCES user_model(id);
-
-ALTER TABLE calendar_id
-    ADD FOREIGN KEY (output_calendar_id) REFERENCES merged_calendar(id);
