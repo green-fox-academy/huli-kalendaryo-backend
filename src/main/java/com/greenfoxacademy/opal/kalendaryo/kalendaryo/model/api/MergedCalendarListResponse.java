9fd42fad60e59252b71092b4abd5294104ef3eff
Tung <tung@Macintosh-MacBook-Pro-9.local>
2018-03-07 16:44:08 +0100

    getcalendar endpoint fixed

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/model/api/MergedCalendarListResponse.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/model/api/MergedCalendarListResponse.java
@@ -6,34 +6,17 @@ import java.util.List;
 
 public class MergedCalendarListResponse {
 
-  private List<MergedCalendarResponse> mergedCalendars;
+    private List<MergedCalendarResponse> mergedCalendars;
 
-  public MergedCalendarListResponse() {
+    public MergedCalendarListResponse() {
   }
 
-  public List<MergedCalendarResponse> getMergedCalendars() {
+    public List<MergedCalendarResponse> getMergedCalendars() {
     return mergedCalendars;
   }
 
-  public void setMergedCalendars(List<MergedCalendarResponse> mergedCalendars) {
-    this.mergedCalendars = mergedCalendars;
-  }
-
-  public void setMergedCalendarResponse(List<MergedCalendar> mergedCalendars) {
-    for (int i = 0; i < mergedCalendars.size(); i++) {
-      MergedCalendarResponse mergedCalendarResponse = new MergedCalendarResponse();
-      mergedCalendarResponse.setOutputAccountId(mergedCalendars.get(i).getOutputAccount());
-      mergedCalendarResponse.setOutputCalendarId(mergedCalendars.get(i).getOutputCalendarId());
-      mergedCalendarResponse.setInputCalendarIds((setToStringCalendarIds(mergedCalendars.get(i))));
-      this.mergedCalendars.add(mergedCalendarResponse);
-    }
-  }
-
-  List<String> setToStringCalendarIds(MergedCalendar mergedCalendar) {
-    List<String> CalendarIDsToString = new ArrayList<>();
-    for (int i = 0; i < mergedCalendar.getCalendarIds().size(); i++) {
-      CalendarIDsToString.add(mergedCalendar.getCalendarIds().get(i).toString());
-    }
-    return CalendarIDsToString;
+    public void setMergedCalendars(List<MergedCalendarResponse> mergedCalendars) {
+        this.mergedCalendars = mergedCalendars;
   }
 }
+
