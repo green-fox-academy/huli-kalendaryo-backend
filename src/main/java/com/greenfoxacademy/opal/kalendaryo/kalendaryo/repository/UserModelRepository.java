b4f16b6a6b2cc909519b588e516ce953dab61633
Balazs Salfay <balazs.salfay@gmail.com>
2018-03-07 13:29:05 +0100

    fix()repository annotation added to usermodel and eventresponse repository

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/repository/UserModelRepository.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/repository/UserModelRepository.java
@@ -3,7 +3,9 @@ package com.greenfoxacademy.opal.kalendaryo.kalendaryo.repository;
 
 import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.entity.UserModel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserModelRepository extends CrudRepository<UserModel, Long> {
 
     UserModel findById(Long id);
