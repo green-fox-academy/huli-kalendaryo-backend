11b7086bc7ffc12d2e0bb62e7006792a1411ef4d
Hieu <hieuci88@gmail.com>
2018-03-21 10:16:04 +0100

    refactor(migration files): V8 updated and plus line at end of files

--- src/main/resources/db/migration/V8__db_update_after_refactor.sql
+++ src/main/resources/db/migration/V8__db_update_after_refactor.sql
@@ -1,17 +1,20 @@
 ALTER TABLE user_model
-  RENAME TO kal_user;
+    RENAME TO kal_user;
 
 ALTER TABLE auth_model
-  RENAME TO google_auth,
-  CHANGE calendar_id google_calendars VARCHAR(255);
+    DROP FOREIGN KEY auth_model_ibfk_2,
+    DROP COLUMN calendar_id,
+    RENAME TO google_auth;
 
 ALTER TABLE event_response
-  RENAME TO google_calendar_update;
+    RENAME TO google_calendar_update;
 
 ALTER TABLE merged_calendar
-  RENAME TO kalendar;
+    RENAME TO kalendar;
 
 ALTER TABLE calendar_id
-  RENAME TO google_calendar,
-  CHANGE auth_model_email google_auth_email VARCHAR(255),
-  CHANGE merged_calendar_id kalendar_id BIGINT;
+    DROP FOREIGN KEY calendar_id_ibfk_1,
+    CHANGE auth_model_email google_auth_email VARCHAR(255),
+    CHANGE merged_calendar_id kalendar_id BIGINT,
+    ADD FOREIGN KEY (google_auth_email) REFERENCES google_auth(email),
+    RENAME TO google_calendar;
