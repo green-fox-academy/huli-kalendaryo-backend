d43b70c1f06d05c6bd028dfe7de0d76420d3c209
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-05-15 17:49:23 +0200

    feat(added findByEmailAndUser query for googleAuthRepository)

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/repository/GoogleAuthRepository.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/repository/GoogleAuthRepository.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.opal.kalendaryo.kalendaryo.repository;
 
 import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.entity.GoogleAuth;
 
+import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.entity.KalUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface GoogleAuthRepository extends CrudRepository<GoogleAuth, String> {
 
     GoogleAuth findByEmail(String email);
+    GoogleAuth findByEmailAndUser(String email, KalUser kalUser);
 }
