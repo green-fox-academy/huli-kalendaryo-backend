0f309ded9b4628d5b99a6c4c774879394fc1ee02
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-05-18 17:20:48 +0200

    Feat(Added new methods needed for Controller refactring)

--- src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/service/AuthAndUserService.java
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/service/AuthAndUserService.java
@@ -1,6 +1,8 @@
 package com.greenfoxacademy.opal.kalendaryo.kalendaryo.service;
 
 import com.google.api.client.util.Base64;
+import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.api.UserResponse;
+import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.api.authresponses.PostAuthResponse;
 import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.entity.GoogleAuth;
 import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.entity.KalUser;
 import com.greenfoxacademy.opal.kalendaryo.kalendaryo.repository.GoogleAuthRepository;
@@ -37,7 +39,7 @@ public class AuthAndUserService{
         saveGoogleAuth(googleAuth);
     }
 
-    public KalUser findUserByClientToken(String clientToken) {
+    private KalUser findUserByClientToken(String clientToken) {
         return kalUserRepository.findByClientToken(clientToken);
     }
 
@@ -48,13 +50,13 @@ public class AuthAndUserService{
         return Base64.encodeBase64String(random);
     }
 
-    public void saveGoogleAuth(GoogleAuth googleAuth) throws IOException {
+    private void saveGoogleAuth(GoogleAuth googleAuth) throws IOException {
         String accessToken = authorization.authorize(googleAuth.getAuthCode());
         googleAuth.setAccessToken(accessToken);
         googleAuthRepository.save(googleAuth);
     }
 
-    public KalUser findUserByUserMail(String email) {
+    private KalUser findUserByUserMail(String email) {
         return kalUserRepository.findByUserEmail(email);
     }
 
@@ -62,16 +64,59 @@ public class AuthAndUserService{
         return kalUserRepository.findKalUserByGoogleAuthListIsContaining(googleAuth);
     }
 
-    public void deleteGoogleAuth(String email, KalUser user) {
+    public void deleteGoogleAuth(String email, String clientToken) {
+        KalUser user = findUserByClientToken(clientToken);
         GoogleAuth googleAuth = googleAuthRepository.findByEmailAndUser_Id(email, user.getId());
         googleAuthRepository.delete(googleAuth);
     }
 
-    public boolean checkIfGoogleAuthExist (GoogleAuth googleAuth, Long kalUserId) {
+
+    private List<GoogleAuth> listAllGoogleAccounts (KalUser user) {
+        return googleAuthRepository.findAllByUser(user);
+    }
+
+    public UserResponse createUserResponseForGetAccounts (String clientToken) {
+        KalUser user = findUserByClientToken(clientToken);
+        List<GoogleAuth> googleAuthList = listAllGoogleAccounts(findUserByClientToken(clientToken));
+        return new UserResponse(user.getId(), user.getUserEmail(), googleAuthList);
+    }
+
+    public boolean checkIfEmailIsLoggedInUser (String email, String clientToken) {
+        KalUser user = findUserByClientToken(clientToken);
+        return user.getUserEmail().equals(email);
+    }
+
+    public PostAuthResponse createPostAuthResponse (String clientToken, GoogleAuth googleAuth) throws IOException{
+        KalUser kalUser = setKalUserForPostAuth(clientToken, googleAuth);
+        googleAuth = manageGoogleAuthForPostAuth(kalUser, googleAuth);
+        return new PostAuthResponse(kalUser.getId(), kalUser.getClientToken(), googleAuth.getEmail(), googleAuth.getAccessToken());
+    }
+
+    private GoogleAuth manageGoogleAuthForPostAuth(KalUser kalUser, GoogleAuth googleAuth) throws IOException{
+        googleAuth.setUser(kalUser);
+        if (checkIfGoogleAuthExist(googleAuth, kalUser.getId())) {
+            googleAuth = googleAuthRepository.findByEmailAndUser_Id(googleAuth.getEmail(),kalUser.getId());
+            return googleAuth;
+        }
+        saveGoogleAuth(googleAuth);
+        return googleAuth;
+    }
+
+    private boolean checkIfGoogleAuthExist (GoogleAuth googleAuth, Long kalUserId) {
         return googleAuthRepository.findByEmailAndUser_Id(googleAuth.getEmail(),kalUserId) != null;
     }
 
-    public List<GoogleAuth> listAllGoogleAccounts (Long id) {
-        return googleAuthRepository.findByUser_Id(id);
+    private KalUser setKalUserForPostAuth (String clientToken, GoogleAuth googleAuth) {
+        KalUser kalUser;
+        if (!clientToken.equals("")) {
+            kalUser = findUserByClientToken(clientToken);
+        } else if (findUserByUserMail(googleAuth.getEmail()) != null) {
+            kalUser = findUserByUserMail(googleAuth.getEmail());
+        } else {
+            kalUser = new KalUser(getRandomClientToken());
+            kalUser.setUserEmail(googleAuth.getEmail());
+        }
+        return kalUser;
     }
+
 }
