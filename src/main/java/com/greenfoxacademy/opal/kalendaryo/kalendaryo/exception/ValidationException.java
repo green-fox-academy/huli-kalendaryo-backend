75ca9249e963d62d78758b24a2ab2c4866f389dc
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-05-23 01:50:03 +0200

    Feat(Added package for exception)

---
+++ src/main/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.opal.kalendaryo.kalendaryo.exception;
+
+public class ValidationException extends Exception {
+  public ValidationException(String message) {
+    super(message);
+  }
+}
