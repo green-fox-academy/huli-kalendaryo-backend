39363fbe5674746880c5e95bb94b98e0887401f4
Bzita <banszegi.zita@gmail.com>
2018-05-31 14:46:37 +0200

    init(OPAL-241) skeleton of end-point and model classes are created

---
+++ src/main/java/com/greenfoxacademy/kalendaryo/model/api/Event.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.kalendaryo.model.api;
+
+public class Event {
+
+  private String eventId;
+
+  public Event () {}
+
+  public String getEventId() {
+    return eventId;
+  }
+
+  public void setEventId(String eventId) {
+    this.eventId = eventId;
+  }
+}
