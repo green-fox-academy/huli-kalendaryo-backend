c918e53cba06afba46ab9ed71821280e19e5e3cb
Bzita <banszegi.zita@gmail.com>
2018-06-06 13:20:20 +0200

    feat(OPAL-95) first test is written

--- src/test/java/com/greenfoxacademy/kalendaryo/controllers/AuthControllerTest.java
+++ src/test/java/com/greenfoxacademy/kalendaryo/controllers/AuthControllerTest.java
@@ -1,43 +1,20 @@
 package com.greenfoxacademy.kalendaryo.controllers;
 
-import com.greenfoxacademy.kalendaryo.exception.ValidationException;
-import com.greenfoxacademy.kalendaryo.model.api.KalendarListResponse;
 import com.greenfoxacademy.kalendaryo.repository.GoogleAuthRepository;
 import com.greenfoxacademy.kalendaryo.repository.KalUserRepository;
-import com.greenfoxacademy.kalendaryo.repository.KalendarRepository;
 import com.greenfoxacademy.kalendaryo.service.AuthAndUserService;
-import com.greenfoxacademy.kalendaryo.service.GoogleCalendarService;
-import com.greenfoxacademy.kalendaryo.service.KalendarService;
-import com.greenfoxacademy.kalendaryo.service.authorization.AuthorizeKal;
-import com.greenfoxacademy.kalendaryo.model.api.KalendarResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.Matchers.anyList;
-import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.core.Is.is;
-
-import static org.junit.Assert.*;
 
 public class AuthControllerTest {
 
@@ -68,8 +45,9 @@ public class AuthControllerTest {
   }
 
   @Test
-  public void notificationEndPointExists() throws Exception {
-    mock.perform(post("/auth"))
-            .andExpect(status().isOk());
+  public void postAuthShouldReturnHttp401WithoutClientToken() throws Exception {
+    mock.perform(post("/auth")
+            .contentType(contentType))
+            .andExpect(status().is4xxClientError());
   }
 }
