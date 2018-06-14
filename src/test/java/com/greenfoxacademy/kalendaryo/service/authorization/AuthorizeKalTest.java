6a5254668f2deffc1a43efd563f24287340992f7
Mark Kovacs <drmarkaron@gmail.com>
2018-06-14 11:09:16 +0200

    test(OPAL-242): init commit for test file

---
+++ src/test/java/com/greenfoxacademy/kalendaryo/service/authorization/AuthorizeKalTest.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.kalendaryo.service.authorization;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+public class AuthorizeKalTest {
+
+  private Mock mock;
+
+  @Test
+  public void createGoogleCalendarUnderAccount() {
+  }
+}
